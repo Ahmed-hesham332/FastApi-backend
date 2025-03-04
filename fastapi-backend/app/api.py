7542ad5cc35605c939d3ac09b572fa586efb83d6
Ahmed Husham <68594545+Ahmed-hesham332@users.noreply.github.com>
2025-03-04 16:15:24 +0300

    Update api.py

--- fastapi-backend/app/api.py
+++ fastapi-backend/app/api.py
@@ -1,13 +1,61 @@
-from fastapi import APIRouter, Depends
-from app.config import settings
-from app.models import AddressResponse
+from fastapi import APIRouter, Depends, HTTPException
+from app.config import settings  
 from app.dependencies import get_api_key
 from app.logger import logger
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
 
-# Define API Router (Make sure it's at the top)
+from fastapi import Request
+
 router = APIRouter()
 
-@router.get("/address", response_model=AddressResponse, dependencies=[Depends(get_api_key)])
-def get_address():
-    logger.info("Address endpoint accessed")
-    return AddressResponse(postcode=settings.POSTCODE)
+recoreds = []
+limit = 50
+offset = 0
+start_date = "2025-01-01"
+end_date = "2025-01-08"
+
+
+configuration = sib_api_v3_sdk.Configuration()
+configuration.api_key["api-key"] = settings.API_KEY  
+api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+        sib_api_v3_sdk.ApiClient(configuration)
+    )
+
+records = [] 
+limit = 50
+offset = 0
+start_date = "2025-01-01"
+end_date = "2025-01-08"
+
+def get_TransactionalEmails():
+    
+    while True:
+        try:
+            response = api_instance.get_email_event_report(
+                limit=limit,
+                offset=offset,
+                start_date=start_date,
+                end_date=end_date,
+            )
+
+            if not response.events:
+                break  
+
+            records.extend(response.events)   
+
+        except ApiException as e:
+            logger.error(f"Error: {e}")
+            return []  
+
+    return records  
+
+
+@router.get("/email-logs", dependencies=[Depends(get_api_key)])
+def export_logs():
+    log = get_TransactionalEmails()
+
+    if not log:
+        return {"message": "No logs available"}  
+
+    return {"logs": log}
